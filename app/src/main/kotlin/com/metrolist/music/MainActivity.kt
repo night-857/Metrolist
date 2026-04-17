5112a2b459e7862bf2e529694bbfd1fa2d58140c
night-857 <yagovictorvs14@gmail.com>
2026-04-17 18:11:14 -0300

    Update MainActivity.kt

--- app/src/main/kotlin/com/metrolist/music/MainActivity.kt
+++ app/src/main/kotlin/com/metrolist/music/MainActivity.kt
@@ -43,6 +43,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.shape.CircleShape
